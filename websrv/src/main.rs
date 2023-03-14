ae7ff3b7e7b498fbd5b100b5d08c58e3f88dcf85
François-Xavier Bourlet <bombela@gmail.com>
2023-03-14 12:42:20 -0700

    CORS allow all in debug

--- websrv/src/main.rs
+++ websrv/src/main.rs
@@ -1,3 +1,9 @@
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    http::Header,
+    Request, Response, serde::Deserialize,
+};
+
 #[macro_use]
 extern crate rocket;
 
@@ -10,7 +16,35 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    let mut rocket = rocket::build();
+
+    #[derive(Deserialize)]
+    #[serde(crate = "rocket::serde")]
+    struct Config {
+        cors_all: bool,
+    }
+
+    let config: Config = rocket.figment().extract().unwrap();
+    if config.cors_all {
+        rocket = rocket.attach(Cors);
+    }
+    rocket
         .mount("/", routes![index])
         .mount("/play", play::routes())
+}
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+    }
 }
